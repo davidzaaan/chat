9fca57cfdd3630c16c6468ffb2c76760032acc10
davidzaaan <odmh2711@gmail.com>
2022-02-08 17:59:21 -0500

    Preparing to deploy #7

--- dj-chat/asgi.py
+++ dj-chat/asgi.py
@@ -1,14 +1,15 @@
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj-chat.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj-chat.settings')
-django.setup()
+
 
 django_asgi_app = get_asgi_application()
 
--- dj-chat/settings.py
+++ dj-chat/settings.py
@@ -10,13 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
+
 from pathlib import Path
 import os
 import django_heroku
+import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj-chat.settings')
+django.setup()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
