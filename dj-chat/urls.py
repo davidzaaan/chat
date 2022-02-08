aba8c3eb2220056d49e3258d4ec4694ce7c9f0a0
davidzaaan <odmh2711@gmail.com>
2022-02-08 16:09:15 -0500

    Preparing for deploy #1

--- dj-chat/urls.py
+++ dj-chat/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+# production
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('chat/', include('chat.urls')),
 ]
+
+# for production purposes...
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
