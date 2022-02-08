9e67f85d43915f4ec5a4d01ff64d29f9a063f529
davidzaaan <odmh2711@gmail.com>
2022-02-08 18:00:48 -0500

    Preparing to deploy #8

--- dj-chat/settings.py
+++ dj-chat/settings.py
@@ -20,8 +20,6 @@ import django
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj-chat.settings')
-django.setup()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
